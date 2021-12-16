2d5616454b2afe51d2c6e57a47d6b32fc0c1123d
Ishaan Kohli <kohliishan@hotmail.com>
2021-12-16 17:57:36 -0600

    Test

--- mfs.c
+++ mfs.c
@@ -112,7 +112,11 @@ int MFS_Write(int inum, char buffer[MFS_BLOCK_SIZE], int block){
 
 	int i;
 	for(i=0; i < MFS_BLOCK_SIZE; i++)
-		req.buffer[i]=buffer[i];
+		req.buffer[i] = buffer[i];
+
+	for(i=0; i < MFS_BLOCK_SIZE; i++)
+		printf("%c", buffer[i]);
+	printf("\n");
 
 	req.inum = inum;
 	req.request = REQ_WRITE;
