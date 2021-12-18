930348dbe0a05ec5d35d09b05a408d04ed39b50d
Ishaan Kohli <kohliishan@hotmail.com>
2021-12-17 18:14:02 -0600

    I hope, it's the final commit

--- client.c
+++ client.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
     // printf("UNLINK2: %d\n", MFS_Unlink(inum, "testfile"));
     // printf("UNLINK3: %d\n", MFS_Unlink(inum, "testdir"));
 
-    // char str[20];
+    char str[20];
 
     // int i;
     // for(i = 0; i < 1794; i++) {
@@ -53,8 +53,53 @@ int main(int argc, char *argv[]) {
     //     printf("Lookup %d: %d\n", i, MFS_Lookup(inum, str));
     // }
 
-    printf("Create %d: %d\n", 1793, MFS_Creat(inum, MFS_REGULAR_FILE, "1793"));
-    printf("Lookup %d: %d\n", 1793, MFS_Lookup(inum, "1793"));
+    int i, j, k;
+    for(i = 0;i < 4;i++) {
+        sprintf(str, "testdir%d", i);
+        printf("Create %d: %d\n", i, MFS_Creat(0, MFS_DIRECTORY, str));
+        int ainum = MFS_Lookup(0, str);
+        printf("Create %d: %d\n", i, MFS_Creat(ainum, MFS_REGULAR_FILE, "file1"));
+        printf("Create %d: %d\n", i, MFS_Creat(ainum, MFS_REGULAR_FILE, "file2"));
+        for(j = 0;j < 3;j++) {
+            sprintf(str, "testdir%d", j);
+            printf("Create %d: %d\n", i, MFS_Creat(ainum, MFS_DIRECTORY, str));
+            int binum = MFS_Lookup(ainum, str);
+            printf("Create %d: %d\n", i, MFS_Creat(binum, MFS_REGULAR_FILE, "file1"));
+            printf("Create %d: %d\n", i, MFS_Creat(binum, MFS_REGULAR_FILE, "file2"));
+            for(k = 0;k < 2;k++) {
+                sprintf(str, "testdir%d", k);
+                printf("Create %d: %d\n", i, MFS_Creat(binum, MFS_DIRECTORY, str));
+                int cinum = MFS_Lookup(binum, str);
+                printf("Create %d: %d\n", i, MFS_Creat(cinum, MFS_REGULAR_FILE, "file1"));
+                printf("Create %d: %d\n", i, MFS_Creat(cinum, MFS_REGULAR_FILE, "file2"));
+            }
+        }
+    }
+
+    for(i = 0;i < 4;i++) {
+        sprintf(str, "testdir%d", i);
+        int ainum = MFS_Lookup(0, str);
+        printf("Lookup Directory: %d\n", ainum);
+        printf("Lookup %d: %d\n", i, MFS_Lookup(ainum, "file1"));
+        printf("Lookup %d: %d\n", i, MFS_Lookup(ainum, "file2"));
+        for(j = 0;j < 3;j++) {
+            sprintf(str, "testdir%d", j);
+            int binum = MFS_Lookup(ainum, str);
+            printf("Lookup Directory: %d\n", binum);
+            printf("Lookup %d: %d\n", i, MFS_Lookup(binum, "file1"));
+            printf("Lookup %d: %d\n", i, MFS_Lookup(binum, "file2"));
+            for(k = 0;k < 2;k++) {
+                sprintf(str, "testdir%d", k);
+                int cinum = MFS_Lookup(binum, str);
+                printf("Lookup Directory: %d\n", cinum);
+                printf("Lookup %d: %d\n", i, MFS_Lookup(cinum, "file1"));
+                printf("Lookup %d: %d\n", i, MFS_Lookup(cinum, "file2"));
+            }
+        }
+    }
+
+    // printf("Create %d: %d\n", 1793, MFS_Creat(inum, MFS_REGULAR_FILE, "1793"));
+    // printf("Lookup %d: %d\n", 1793, MFS_Lookup(inum, "1793"));
 
 
     MFS_Shutdown();
